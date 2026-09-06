5e635f7a0cee7a2503efedcaceb764313bf59eb7
Asura <asurahurbanova@gmail.com>
2026-09-06 11:36:20 +0200

    feat: implement express server

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import pinoHttp from 'pino-http';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(pinoHttp());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((_req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
