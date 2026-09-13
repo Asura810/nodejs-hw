ffc32a5305355536611e1fa8b77dbc00e159f76a
Asura <asurahurbanova@gmail.com>
2026-09-13 17:00:18 +0200

    move pino logger

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import pinoHttp from 'pino-http';
+
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(pinoHttp());
+app.use(logger);
 
 const PORT = process.env.PORT || 3000;
 
