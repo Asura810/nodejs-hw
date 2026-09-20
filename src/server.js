05127af30f6126f512d6e232a448ce640981fafa
Asura <asurahurbanova@gmail.com>
2026-09-20 13:24:40 +0200

    Fix

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
-import { authenticate } from './middleware/authenticate.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
@@ -23,7 +22,7 @@ app.use(logger);
 const PORT = process.env.PORT || 3000;
 
 app.use(authRoutes);
-app.use('/notes', authenticate, notesRoutes);
+app.use(notesRoutes);
 app.use(userRoutes);
 
 app.use(errors());
