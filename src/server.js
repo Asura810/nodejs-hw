3d9a08fc47b1725fa5e93eb205cd74711b26495d
Asura <asurahurbanova@gmail.com>
2026-09-20 13:33:54 +0200

    Reorder middleware in server.js

--- src/server.js
+++ src/server.js
@@ -7,19 +7,18 @@ import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
-import userRoutes from './routes/userRoutes.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(logger);
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(logger);
-
-const PORT = process.env.PORT || 3000;
 
 app.use(authRoutes);
 app.use(notesRoutes);
